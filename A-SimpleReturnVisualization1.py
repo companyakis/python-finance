0b6303b5e941afb24c6b4c7ccd1fb02e455d983a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-01-03 15:33:46 +0300

    Create A-SimpleReturnVisualization1.py

---
+++ A-SimpleReturnVisualization1.py
@@ -0,0 +1,13 @@
+# simple return visualization
+
+import matplotlib.pyplot as plt
+
+garanti['simple return'].plot(figsize = (15, 8))
+
+plt.title("Garanti Simple Return")
+
+plt.xlabel("Years")
+
+plt.ylabel("Simple Return")
+
+plt.show()
